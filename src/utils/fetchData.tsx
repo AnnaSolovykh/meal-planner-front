d01ed937f42a91577afda99273f225fc11835a0e
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-02-06 18:20:35 +0300

    update fetching meal with interceptors

--- src/utils/fetchData.tsx
+++ src/utils/fetchData.tsx
@@ -1,41 +1,38 @@
-import axios from 'axios';
+import api from './api';
 import { MealsType, SavedMealType } from './types';
 
-const jwtToken = localStorage.getItem('jwtToken');
-
-export const login = (email: string, password: string) => {
-    return axios.post(`http://localhost:4000/api/v1/auth/login`, 
+export const fetchLogin = (email: string, password: string) => {
+    return api.post(`http://localhost:4000/api/v1/auth/login`, 
         {
-            email: email,
-            password: password
+            email,
+            password,
         },
         {
             headers: {
-                'Content-Type': 'application/json'
-            }
+                'Content-Type': 'application/json',
+            },
         }
     );
 };
 
-export const register = (name: string, email: string, password: string) => {
-    return axios.post(`http://localhost:4000/api/v1/auth/register`, 
+export const fetchRegister = (name: string, email: string, password: string) => {
+    return api.post(`http://localhost:4000/api/v1/auth/register`, 
         {
-            name: name,
-            email: email,
-            password: password
+            name,
+            email,
+            password,
         },
         {
             headers: {
-                'Content-Type': 'application/json'
-            }
+                'Content-Type': 'application/json',
+            },
         }
     );
 };
 
-export const logout = () => {
-    return axios.post('http://localhost:4000/api/v1/auth/logout');
-};  
-
+export const fetchLogout = () => {
+    return api.post('http://localhost:4000/api/v1/auth/logout');
+};
 
 export const getMeals = (typeFilter: string, titleFilter: string, isFavoriteFilter: boolean, currentPage: number, limit: number): Promise<MealsType> => {
     let query = `page=${currentPage}&limit=${limit}`;
@@ -50,51 +47,38 @@ export const getMeals = (typeFilter: string, titleFilter: string, isFavoriteFilt
         query += `&isFavorite=${isFavoriteFilter}`;
     }
 
-    return axios.get(`http://localhost:4000/api/v1/meals?${query}`, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${jwtToken}`
-        }
-    })
+    return api.get(`http://localhost:4000/api/v1/meals?${query}`)
     .then(response => response.data as MealsType);
 };
 
-
 export const createMeal = (meal: SavedMealType) => {
-    return axios.post('http://localhost:4000/api/v1/meals/',
-    {
-        ...meal
-    },
+    return api.post('http://localhost:4000/api/v1/meals/',
+    meal,
     {
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${jwtToken}`
-        }
+        },
     }
-    )
+    );
 };
 
 export const updateMeal = (mealId: string, meal: SavedMealType) => {
-    return axios.patch(`http://localhost:4000/api/v1/meals/${mealId}`, 
-        {
-            ...meal
+    return api.patch(`http://localhost:4000/api/v1/meals/${mealId}`, 
+    meal,
+    {
+        headers: {
+            'Content-Type': 'application/json',
         },
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${jwtToken}`
-            }
-        }
-    )
+    }
+    );
 };
 
 export const deleteMeal = (mealId: string) => {
-    return axios.delete(`http://localhost:4000/api/v1/meals/${mealId}`, 
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${jwtToken}`
-            }
-        }
-    )
+    return api.delete(`http://localhost:4000/api/v1/meals/${mealId}`,
+    {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+    );
 };
